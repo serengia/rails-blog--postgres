4021c86554ad1d245b8cd9a915d320617c4ebaf2
James Serengia <jamesserengia@gmail.com>
2023-05-12 17:02:43 +0300

    Add single user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,8 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
-  def show; end
+  def show
+    @user = User.find(params[:user_id])
+    @posts = Post.all
+  end
 end
