a31c510a505bea0055617ffd428455f718571ae6
James Serengia <jamesserengia@gmail.com>
2023-05-13 20:18:14 +0300

    Implement create comment option

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_04_182723) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_13_065945) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_04_182723) do
     t.bigint "posts_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "post_id"
     t.index ["author_id"], name: "index_comments_on_author_id"
     t.index ["posts_id"], name: "index_comments_on_posts_id"
   end
