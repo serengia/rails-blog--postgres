b505a6999ff1543fc4b0e7c2666cb1738a7cdea3
James Serengia <jamesserengia@gmail.com>
2023-05-13 07:31:14 +0300

    Update post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,23 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = @current_user.posts.new
+  end
+
+  def create
+    @post = @current_user.posts.new(post_params)
+    if @post.save
+      redirect_to user_posts_path(@current_user)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
