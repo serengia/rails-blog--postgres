b74a1bdd3e9091b2ad10551449cfe388523492d5
James Serengia <jamesserengia@gmail.com>
2023-05-13 07:28:23 +0300

    Update AppController and refactored routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,22 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root 'users#index'
-  get '/users', to: 'users#index'
-  get "/users/:user_id", to: "users#show"
-  get "/users/:user_id/posts", to: "posts#index"
-  get "/users/:user_id/posts/:post_id", to: "posts#show"
-  get '/login', to: 'pages#login'
+  # root 'users#index'
+  # get '/users', to: 'users#index'
+  # get "/users/:user_id", to: "users#show"
+  # get "/users/:user_id/posts", to: "posts#index"
+  # get "/users/:user_id/posts/:post_id", to: "posts#show"
+  # get '/login', to: 'pages#login'
+
   
+  # REFACTORED
+    resources :users, only: [:index, :show] do
+      resources :posts, only: [:index, :show, :new, :create] do
+        resources :comments, only: [:new, :create]
+        resources :likes, only: [:create]
+      end
+    end
+    get '/login', to: 'pages#login'
+    root "users#index"
+
 end
